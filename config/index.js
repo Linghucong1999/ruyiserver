3a46c8f2ca632c64d8b8bd421dcce0861f8711ce
mika <2865911620@qq.com>
2023-11-13 23:19:37 +0800

    数据库配置等各系列配置文件

---
+++ config/index.js
@@ -0,0 +1,11 @@
+module.exports = {
+    port: 8018,
+    mongodb: {
+        url: 'mongodb://localhost:27017/ruyidatabase',
+        options: {}
+    },
+    middleware: ['handlerError'],
+    jwt: { secret: 'linghucong1998' },
+    crypto: { secret: '#*#*linghucong1998*#*#' },
+    baseUrl: ''
+}
