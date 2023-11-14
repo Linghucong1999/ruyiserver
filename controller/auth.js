736568dca166fbd02d93a0e9ac79706885f4ac10
卢吉生 <2865911620@qq.com>
2023-11-14 18:27:58 +0800

    登录控制器

--- controller/auth.js
+++ controller/auth.js
@@ -5,7 +5,13 @@ module.exports = app => ({
      */
 
     async login() {
-        const { ctx } = app;
+        const { ctx,$service } = app;
         const { username, password } = ctx.request.body;
+        console.log($service)
+        //验证数据库是否存在
+        let user=await $service.user.getUsersByUsername(username);
+        if(!user){
+
+        }
     }
 })
