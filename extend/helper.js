58723ff8a255ea93f341c4b1ac852144635990c9
卢吉生 <2865911620@qq.com>
2023-11-17 15:42:14 +0800

    完善拓展

--- extend/helper.js
+++ extend/helper.js
@@ -34,4 +34,72 @@ module.exports = app => ({
         let { $config } = app;
         return await jwt.verify(token, $config.jwt.secret);
     },
-})
+
+    //用户密码加密
+    async createPassword(password) {
+        let { $config } = app;
+        const salt = await bcryptjs.genSalt($config.bcrypt.saltRounds);
+        return await bcryptjs.hash(password, salt);
+    },
+
+    //验证密码
+    async verifyPassword(password, hash_password) {
+        return await bcryptjs.compare(password, hash_password);
+    },
+
+    /**
+     * 检测目录是否存在，不存在就创建一个新目录
+     */
+    async dirExists(dir) {
+        let { $helper } = app;
+        let isExists = await getStat(dir);
+        //如果路径存在，且不是文件，返回true
+        if (isExists && isExists.isDirectory()) {
+            return true;
+        } else if (isExists) {        //如果路径存在，但是是文件，返回false
+            return false;
+        }
+
+        //如果路径不存在
+        let tempDir = path.parse(dir).dir;    //拿到上级路径
+        //递归判断，如果上级目录不存在，则代码会在此处继续循环执行，直到目录存在为止，没有的目录都会一直创建
+        let status = await $helper.dirExists(tempDir);
+        let mkdirStatus;
+        if (status) mkdirStatus = await mkdir(dir);
+        return mkdirStatus;
+    },
+});
+
+
+/**
+ * 读取路径信息
+ * @param {string} path //路径
+ */
+function getStat(path) {
+    return new Promise((resolve, reject) => {
+        fs.stat(path, (err, stats) => {
+            if (err) {
+                resolve(false);
+            } else {
+                resolve(stats);
+            }
+        })
+    })
+}
+
+/**
+ * 创建路径
+ * @param {string} dir  //目录
+ */
+
+function mkdir(dir) {
+    return new Promise((resolve, reject) => {
+        fs.mkdir(dir, err => {
+            if (err) {
+                resolve(false);
+            } else {
+                resolve(true);
+            }
+        })
+    })
+}
