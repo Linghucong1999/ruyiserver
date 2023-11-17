24b337421e2239146152640973cb86c2df61f815
卢吉生 <2865911620@qq.com>
2023-11-17 15:41:01 +0800

    继续完善登录接口

--- controller/auth.js
+++ controller/auth.js
@@ -5,13 +5,24 @@ module.exports = app => ({
      */
 
     async login() {
-        const { ctx,$service } = app;
+        const { ctx, $service, $helper } = app;
         const { username, password } = ctx.request.body;
         console.log($service)
         //验证数据库是否存在
-        let user=await $service.user.getUsersByUsername(username);
-        if(!user){
-
+        let user = await $service.user.getUsersByUsername(username);
+        if (!user) {
+            $helper.returnBody(false, {}, '用户不存在');
+            return;
         }
+
+        //校验密码
+        const userCurrentPassword = await $service.user.getUsersByUsernameCompare(username);
+        const verifyPass = await $helper.verifyPassword(password, userCurrentPassword.password);
+        if (!verifyPass) {
+            $helper.returnBody(false, '', "密码错误，请重试!");
+            return;
+        }
+
+        user=user
     }
 })
