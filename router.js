8249491267000e0fc2cf96595cc6ebe066e3b356
卢吉生 <2865911620@qq.com>
2023-11-20 15:43:50 +0800

    新增注册API

--- router.js
+++ router.js
@@ -1,5 +1,6 @@
 module.exports = app => {
     const { router, $controller } = app;
     //登录注册认证
-    router.post('/ruyi/auth/login',$controller.auth.login);
+    router.post('/ruyi/auth/login', $controller.auth.login);
+    router.post('/ruyi/auth/register', $controller.auth.register);
 }
