88e0a23d7e358bc3fe819a890173ea29494b52e0
卢吉生 <2865911620@qq.com>
2023-12-05 17:32:18 +0800

    feat:新增用户路由

--- router.js
+++ router.js
@@ -1,6 +1,14 @@
 module.exports = app => {
-    const { router, $controller } = app;
-    //登录注册认证
-    router.post('/ruyi/auth/login', $controller.auth.login);
-    router.post('/ruyi/auth/register', $controller.auth.register);
+    const { router, controller, middleware } = app;
+    //登录注册
+    router.post('/ruyi/auth/login', controller.auth.login);
+    router.post('/ruyi/auth/register', controller.auth.register);
+
+    //用户
+    router.get('/ruyi/user/info', middleware.auth, controller.user.getUserinfo);
+    router.post('/ruyi/user/updata/name', middleware.auth, controller.user.updataUserName);
+    router.post('/ruyi/user/updata/password', middleware.auth, controller.user.updataPassword);
+    router.post('/ruyi/user/updata/avater', middleware.auth, controller.user.updataAvatar);
+    router.get('/ruyi/user/getUserList', middleware.auth, controller.user.fuzzyQueryUserList);
+
 }
